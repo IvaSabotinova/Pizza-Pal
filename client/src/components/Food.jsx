86640a1a8636f8161f1aca681c4e02ff55ca63e4
IvaSabotinova <96121572+IvaSabotinova@users.noreply.github.com>
2023-11-12 01:08:04 +0200

    Load more functionality

--- client/src/components/Food.jsx
+++ client/src/components/Food.jsx
@@ -6,6 +6,7 @@ import * as productService from '../services/productService';
 export default function Food() {
     const [products, setProducts] = useState([]);
     const [activeFilter, setActiveFilter] = useState('*');
+    const [visibleProducts, setVisibleProducts] = useState(6);
 
     useEffect(() => {
         productService.getAll()
@@ -15,9 +16,15 @@ export default function Food() {
 
     const filterProducts = (type) => {
         setActiveFilter(type);
+        setVisibleProducts(6);
     }
 
-    const filteredProducts = activeFilter === '*' ? products : products.filter(x=>x.type === activeFilter);
+    const filteredProducts = activeFilter === '*' ? products : products.filter(x => x.type === activeFilter);
+
+    const loadMore = (e) => {
+        e.preventDefault() //Prevent the default behavior of scrolling to the top
+        setVisibleProducts(state => state + 6);
+    };
 
     return (<section className="food_section layout_padding-bottom">
         <div className="container">
@@ -25,15 +32,15 @@ export default function Food() {
                 <h2>Our Menu</h2>
             </div>
             <ul className="filters_menu">
-                <li className={activeFilter === '*' ? 'active': ''} onClick={()=> filterProducts('*')} data-filter="*"> All </li>
-                <li className={activeFilter === 'pizza' ? 'active' : ''} onClick={()=> filterProducts('pizza')} data-filter=".pizza">Pizzas</li>
-                <li className={activeFilter === 'starter' ? 'active': ''} onClick={()=> filterProducts('starter')} data-filter=".starter">Starters</li>
-                <li className={activeFilter==='dessert'? 'active': ''} onClick={()=> filterProducts('dessert')} data-filter=".dessert">Desserts</li>
-                <li className={activeFilter==='drink' ? 'active' : ''} onClick={()=> filterProducts('drink')} data-filter=".drink">Drinks</li>
+                <li className={activeFilter === '*' ? 'active' : ''} onClick={() => filterProducts('*')} data-filter="*"> All </li>
+                <li className={activeFilter === 'pizza' ? 'active' : ''} onClick={() => filterProducts('pizza')} data-filter=".pizza">Pizzas</li>
+                <li className={activeFilter === 'starter' ? 'active' : ''} onClick={() => filterProducts('starter')} data-filter=".starter">Starters</li>
+                <li className={activeFilter === 'dessert' ? 'active' : ''} onClick={() => filterProducts('dessert')} data-filter=".dessert">Desserts</li>
+                <li className={activeFilter === 'drink' ? 'active' : ''} onClick={() => filterProducts('drink')} data-filter=".drink">Drinks</li>
             </ul>
             <div className="filters-content">
                 <div className="row grid">
-                    {filteredProducts.map(prod => (<Product
+                    {filteredProducts.slice(0, visibleProducts).map(prod => (<Product
                         key={prod.id}
                         {...prod}
 
@@ -762,9 +769,10 @@ export default function Food() {
                     </div> */}
                 </div>
             </div>
-            <div className="btn-box">
+            {visibleProducts < filteredProducts.length && 
+            <div className="btn-box" onClick={loadMore}>
                 <a href="">View More</a>
-            </div>
+            </div>}
         </div>
     </section>);
 }
